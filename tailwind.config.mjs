c649fbb1e06730fe6bff614d6c93690831000a01
sylvainblanchoud77-png <sylvainblanchoud77@gmail.com>
2025-11-01 14:31:13 +0100

    Delete tailwind.config.mjs

--- tailwind.config.mjs
+++
@@ -1,6 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./src/**/*.{astro,html,js,jsx,ts,tsx,md,mdx}', './public/**/*.html'],
-  theme: { extend: {} },
-  corePlugins: { preflight: false }
-}
